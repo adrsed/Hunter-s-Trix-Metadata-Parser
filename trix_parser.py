46cfdea5a385a3cc0c639f609396cf94b7bd074f
Adrian <adrian.seddig@stud.uni-hannover.de>
2024-12-15 15:01:53 +0100

    arguments now take priority over automatically searched files, added automatic artist parsing and argument for volume number

--- trix_parser.py
+++ trix_parser.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("dir", nargs='?')  # album directory
 parser.add_argument("-m", "--metadata", help="File containing the metadata")  # metadata file
 parser.add_argument("-c", "--cover", help="Album Cover")  # album cover
-parser.add_argument("-a", "--artist", default="Grateful Dead")
+parser.add_argument("-a", "--artist")
+parser.add_argument("-v", "--volume")
 parser.add_argument("-g", "--genre", default="Rock")
 
 args = parser.parse_args()
@@ -19,60 +20,67 @@ if args.dir is None:
     print("No input directory given. Exiting.")
     sys.exit(1)
 
-artist = args.artist
 genre = args.genre
 
 album_dir = args.dir
 
-# search for metadata file
-# filename looks like "gd73-06-22.mtx.seamons.txt"
-metadata = [f for f in os.listdir(album_dir) if re.match(r".*\.mtx\.seamons\.txt", f)]
-
-if len(metadata) == 0:
-    if args.metadata is None:
-        print("Failed to locate metadata file, place it in the correct directory or provide a path using -m flag. "
-              "Exiting")
+# Search for metadata file or take from arguments if provided.
+# Filename looks like "gd73-06-22.mtx.seamons.txt".
+if args.metadata is not None:
+    metadata = args.metadata
+    if not os.path.isfile(metadata):
+        print(f"Provided metadata file is can not be found. Exiting.")
         sys.exit(1)
-    else:
-        metadata = args.metadata
-        metadata = os.path.split(metadata)[1]  # remove directory prefix, as it would break the script later on
+
+    metadata = os.path.split(metadata)[1]  # remove directory prefix, as it would break the script later on
 else:
+    metadata = [f for f in os.listdir(album_dir) if re.match(r".*\.mtx\.seamons\.txt", f)]
+    if len(metadata) == 0:
+        print("Failed to locate metadata file, place it in the correct directory or provide a path using -m flag. "
+              "Exiting.")
+        sys.exit(1)
+
     metadata = metadata[0]
 
 print(f"Metadata file: {metadata}")
 
-# locate cover file
-# Cover image is usually named something like "gd730622_front1.jpg"
-# sometimes there are more than one image, use the first one found in that case
-cover = [f for f in os.listdir(album_dir) if re.match(r".*_front*.\.jpg", f)]
+# Locate cover file or take from arguments if provided.
+# Cover image is usually named something like "gd730622_front1.jpg".
+# Sometimes there are more than one image, use the first one found in that case.
+if args.cover is not None:
+    cover = args.cover
+    if not os.path.isfile(cover):
+        print(f"Provided cover file is can not be found. Exiting.")
+        sys.exit(1)
 
-if len(cover) == 0:
-    if args.cover is None:
+    cover = os.path.split(cover)[1]  # remove directory prefix, as it would break the script later on
+else:
+    cover = [f for f in os.listdir(album_dir) if re.match(r".*_front*.\.jpg", f)]
+    if len(cover) == 0:
         print("Failed to locate cover image, place it in the correct directory or provide a path using -c flag. "
               "Exiting.")
         sys.exit(1)
-    else:
-        cover = args.cover
-        cover = os.path.split(cover)[1]  # remove directory prefix, as it would break the script later on
-else:
+
     cover = cover[0]
 
 print(f"Cover file: {cover}")
 cover = os.path.join(album_dir, cover)
 
-# extract volume number from directory name
-# if unchanged, the directory name looks something like "gd73-06-22.mtx.seamons.ht12.92375.flac16" with the "ht12"
-# denoting the volume number, Volumes 82-94 were labeled as "Jubal's Trix", so the notation is with a "j" instead of "h"
-vol = re.search(r"\.[hj]t[0-9]*\.", album_dir)
-if vol is None:
-    vol = input("Could not extract Volume from directory name, please enter: ")
+# Extract volume number from directory name.
+# If unchanged, the directory name looks something like "gd73-06-22.mtx.seamons.ht12.92375.flac16" with the "ht12".
+# denoting the volume number, Volumes 82-94 were labeled as "Jubal's Trix", so the notation is with a "j" instead.
+if args.volume is not None:
+    vol = args.volume
 else:
-    vol = re.sub('[.hjt]', '', vol.group(0))
+    vol = re.search(r"\.[hj]t[0-9]*\.", album_dir)
+    if vol is None:
+        vol = input("Could not extract Volume from directory name, please enter: ")
+    else:
+        vol = re.sub('[.hjt]', '', vol.group(0))
 
 print()
 
-
-# ==========================================================
+# ======================================================================================================================
 # Read Metadata file
 
 # First four lines of the metadat file look something like this:
@@ -81,6 +89,13 @@ print()
 # Vancouver, BC, Canada
 # June 22, 1973
 lines = [line.strip() for line in open(os.path.join(album_dir, metadata), "rt")]
+
+# Take artist from command or the file
+if args.artist is not None:
+    artist = args.artist
+else:
+    artist = lines[0]
+
 venue = lines[1]
 loc = lines[2]
 date = lines[3]
@@ -96,7 +111,7 @@ album = f"{date} - {venue} (Hunter's Trix Vol. {vol})"
 print(album)
 print()
 
-# filter metadata file for tracks
+# Filter metadata file for tracks
 # Track lines look like "d1t01 - Bertha"
 lines = [line for line in lines if " - " in line and line[0] == 'd']
 
@@ -106,7 +121,7 @@ for line in lines:
     # Identifier is "d1t01"
     identifier = re.search(r"^d[0-9]t[0-9]*", line).group(0)
 
-    # the number after the 'd' is the disc number
+    # The number after the 'd' is the disc number
     disc = identifier[1]  # this wouldn't work if there are over 9 discs, but there probably won't be
 
     # Track number is anything after the 't', remove that and any leading 0s
